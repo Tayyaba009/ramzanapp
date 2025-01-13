f103ad4dddb86bf3f5dbfd983039b24b79623ffd
noreen <81028088+Tayyaba009@users.noreply.github.com>
2025-01-13 01:43:28 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,3 +2,17 @@
     include_once 'api/apis.php';
     $db = new MysqliDb ('localhost', 'root', '12345678910', 'assignments4');
     ?>
+<?php 
+    include_once 'apis/MysqliDb.php';
+    
+    // Creating a new database connection
+    $db = new MysqliDb(
+        'database11.mysql.database.azure.com', // Host
+        'ramzan',                   // Username
+        'Ramzan123@',                       // Password
+        'assignment4'                   // Database name
+    );
+    
+    // Debugging the database connection object
+    var_dump($db);
+?>
